cbe14a58d5b463246b26548625b5472274145e28
sazed1237 <sazedulislam9126@gmail.com>
2024-02-18 14:30:17 +0600

    deploy vercel

---
+++ ___server_heroku_deploy.js
@@ -0,0 +1,13 @@
+/**
+ * ONE TIME for one time computer
+ * ---------------------
+ * 1. create heroku account
+ * 2. verify email
+ * 3. instal heroku cli
+ * 4. heroku login
+ * --------------------------
+ * 
+ * fore each 
+ * 
+ * 
+ * */ 
